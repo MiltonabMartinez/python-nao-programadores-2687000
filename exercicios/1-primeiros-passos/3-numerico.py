c48d9bfefcef24f880158b41a9cf25aa61fc8a5c
MiltonabMartinez <mamartinez4@stefanini.com>
2025-08-25 14:45:43 +0000

    Numerico alteracao

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,11 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
-
+print(type(data_nascimento))
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+Soma_w = idade_numerica +1
+print(Soma_w)
 # Realize uma operação entre dados do tipo inteiro e float
+Decimal=30/8
+Resultado = Soma_w+int(Decimal)
+print (Resultado)
