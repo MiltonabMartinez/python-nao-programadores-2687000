f5b6704b5980e6fce3e51627fd3bdd5a21956c15
MiltonabMartinez <mamartinez4@stefanini.com>
2025-08-27 15:37:33 +0000

    alteracaoestruturacondicional

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,7 +1,16 @@
 # Declare 4 variáveis do tipo numérica
-
+x=0
+y=2
+z=7
+a=3
 
 # Crie uma estrutura condicional para comparar dois números
+if x > y or ( x>a):
+     print(f'O numero {x} e maior que {y} ou maior do que {a}')
+elif (x == a ) and (z > y) :
+   print(f'Nesse caso , verificamos que o numero {x} e igual a {a} e {z} maior do que {y}')
+else :
+  print(f'O numero {x} nao e maior que {y}')
 
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
