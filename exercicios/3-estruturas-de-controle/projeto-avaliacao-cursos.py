830ac14e661594fa0c2b01aeb53212b3c3644634
MiltonabMartinez <mamartinez4@stefanini.com>
2025-08-28 13:02:59 +0000

    alteracao_projetoavaliaca-cursos

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,23 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+cursos_linkedin= ['Introduçao a SQL ' ,'ITIL','Python' ,'Formaçao Basica','Fundamentos de Programacao']
+curso_git = 'Git e Git Hub: Formacao Basica'
+curso_python = 'Python'
+curso_itil = 'ITIL'
+
+avaliacoes ={}
+if curso_python in cursos_linkedin :
+   print(f"O curso esta no catalogo , Por favor avalie o curso")
+   avaliacoes [curso_python] = int(input('Qual é a nota que voce da para o curso (0 a 5)?'))
+
+if curso_git in cursos_linkedin :
+  print(f"O curso esta no catalogo , Por favor avalie o curso")
+  avaliacoes [curso_git] = int(input('Qual é a nota que voce da para o curso (0 a 5)?'))
+
+if curso_itil in cursos_linkedin :
+  print("Infelizmente o curso não compoe nosso catalogo")
+  avaliacoes [curso_itil] = int(input('Qual é a nota que voce da para o curso (0 a 5)?'))
+else:
+   print("Infelizmente o curso nao compoe nosso catalogo")
+print (avaliacoes)
